666a134d1f6ddac6c0c8e4085ca8e4bb1e2a10af
jpernia15 <yaonlivvonce287@gmail.com>
2025-01-27 17:49:01 -0400

    carpeta spring initializr

---
+++ GasolineraCasu/src/test/java/com/CasuBrito/GasolineraCasu/GasolineraCasuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CasuBrito.GasolineraCasu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasolineraCasuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
